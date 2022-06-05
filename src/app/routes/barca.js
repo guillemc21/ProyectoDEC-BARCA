20416f379373e11591ece8f1c0fe99c22d8640a1
Guille <57747085+Byguillemc21@users.noreply.github.com>
2022-06-05 22:03:25 +0200

    dec
    dec

---
+++ src/app/routes/barca.js
@@ -0,0 +1,34 @@
+const dbConnection = require('../../config/dbConnection');
+/* 
+--> redirect(poner ruta que coincida con alguna indicada con el app.get(...))
+--> render(poner ruta de archivo ejs para renderizar)
+*/
+module.exports = app => {
+
+    const connection = dbConnection();
+
+    app.get('/', (req, res) => {
+        
+        res.render('barca/index');
+        
+    });
+
+    app.get('/barca', (req, res) => {
+        connection.query('SELECT * FROM reservas', (err, result) => {
+            console.log(result);
+            res.render('barca/barca', {
+                news: result
+            });
+        });
+    });
+
+    app.post('/barca2', (req, res) => {
+        const { nombre , apellido } = req.body;
+        connection.query('INSERT INTO reservas SET?', {
+            nombreUsuario: nombre,
+            apellidoUsuario: apellido
+        }, (err, result) => {
+            res.redirect('/barca')
+        });
+    });
+}
---
+++ src/sql/database.sql
@@ -0,0 +1,37 @@
+
+/* Guille Mendoza || Pau Motos */
+DROP DATABASE proyectoBarca;
+CREATE DATABASE proyectoBarca;
+USE proyectoBarca;
+
+CREATE TABLE usuarios 
+(id_usuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+Nombre VARCHAR (25),
+Usuario VARCHAR (15),
+Apellido VARCHAR (20),
+Fecha_Nacimiento DATE,
+Gmail VARCHAR(50),
+Contrasenya VARCHAR (15)
+);
+
+CREATE TABLE reservas
+(id_reserva INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nombreUsuario VARCHAR (25),
+apellidoUsuario VARCHAR (20),
+correoUsuario VARCHAR (50),
+telefono INT,
+fechaReserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fecha DATE,
+asiento INT
+);
+
+DESCRIBE reservas;
+
+INSERT INTO usuarios VALUES
+(1,"Guille","gmendo","Mendoza","2001-05-07","gmendo@gmail.com","P@ssw0rd"),
+(2,"Pau","pmotos","Motos","2001-02-01","pmotos@gmail.com","P@ssw0rd");
+
+INSERT INTO reservas(nombreUsuario,apellidoUsuario) VALUES
+('Guille','Mendoza Castro');
+
+SELECT * FROM reservas;
