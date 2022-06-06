64513e3dca8e3e9b9335fcc852700597b9b7d2e9
Guille <57747085+Byguillemc21@users.noreply.github.com>
2022-06-06 19:55:14 +0200

    dec
    dec

--- src/config/server.js
+++ src/config/server.js
@@ -16,5 +16,8 @@ app.set('views', path.join(__dirname, '../app/views'));
 /* middleware */
 app.use(bodyParser.urlencoded({extended: false}));
 
+/* public */
+app.use(express.static(path.join(__dirname, '../public/img')));
+
 module.exports = app;
 
