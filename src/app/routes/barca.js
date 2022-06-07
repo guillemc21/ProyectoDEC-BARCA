06ad6103a3fb9b7be7cf2665f2658406e00201f9
paumotos <91218054+paumotos@users.noreply.github.com>
2022-06-07 20:25:43 +0200

    sadsd

--- src/app/routes/barca.js
+++ src/app/routes/barca.js
@@ -1,50 +1,59 @@
-const dbConnection = require('../../config/dbConnection');
+const dbConnection = require("../../config/dbConnection");
 /* 
 --> redirect(poner ruta que coincida con alguna indicada con el app.get(...))
 --> render(poner ruta de archivo ejs para renderizar)
 */
-module.exports = app => {
+module.exports = (app) => {
+  const connection = dbConnection();
 
-    const connection = dbConnection();
+  app.get("/", (req, res) => {
+    res.render("barca/index");
+  });
 
-    app.get('/', (req, res) => {
-        
-        res.render('barca/index');
-        
+  app.get("/barca", (req, res) => {
+    connection.query("SELECT * FROM reservas", (err, result) => {
+      console.log(result);
+      res.render("barca/barca", {
+        news: result,
+      });
     });
+  });
 
-    app.get('/barca', (req, res) => {
-        connection.query('SELECT * FROM reservas', (err, result) => {
-            console.log(result);
-            res.render('barca/barca', {
-                news: result
-            });
-        });
-    });
+  app.post("/barca2", (req, res) => {
+    const { nombre, apellido, correo, telefono, fecha, ruta } = req.body;
+    connection.query(
+      "INSERT INTO reservas SET?",
+      {
+        nombreUsuario: nombre,
+        apellidoUsuario: apellido,
+        correoUsuario: correo,
+        telefono: telefono,
+        fechaReserva: fecha,
+        RUTA: ruta,
+      },
+      (err, result) => {
+        res.redirect("/barca");
+      }
+    );
+  });
 
-    app.post('/barca2', (req, res) => {
-        const { nombre , apellido , correo , telefono , fecha , ruta} = req.body;
-        connection.query('INSERT INTO reservas SET?', {
-            nombreUsuario: nombre,
-            apellidoUsuario: apellido,
-            correoUsuario: correo,
-            telefono: telefono,
-            fechaReserva: fecha,
-            RUTA: ruta
-        }, (err, result) => {
-            res.redirect('/barca')
-        });
-    });
+  app.get("/covid", (req, res) => {
+    res.render("barca/covid");
+  });
 
-    app.get('/covid', (req, res) => {
+  app.get("/cancel", (req, res) => {
+    res.render("barca/cancel");
+  });
 
-        res.render('barca/covid');
+  app.get("/alcudia", (req, res) => {
+    res.render("barca/alcudia");
+  });
 
-    });
+  app.get("/palma", (req, res) => {
+    res.render("barca/palma");
+  });
 
-    app.get('/cancel', (req, res) => {
-        
-        res.render('barca/cancel');
-
-    });
-}
+  app.get("/ciutadella", (req, res) => {
+    res.render("barca/ciutadella");
+  });
+};
